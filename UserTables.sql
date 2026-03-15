6114ecaacc97d18428420517715473bd33b84561
l8ralig8r <hanan.ali@maine.edu>
2026-03-15 17:53:29 -0400

    Added User and User_Subject tables

---
+++ UserTables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE User(
+	user_ID BINARY(16) NOT NULL PRIMARY KEY
+        DEFAULT (UUID_TO_BIN(UUID())),
+	first_name VARCHAR(32),
+    last_name VARCHAR(32),
+    email VARCHAR(255) UNIQUE  NOT NULL,
+    phone_number VARCHAR(20)
+);
+
+CREATE TABLE User_Subject(
+	user_ID BINARY(16),
+	subject_ID BINARY(16),
+	user_type ENUM('Sought', 'Offered') NOT NULL,
+    
+PRIMARY KEY(user_ID, subject_ID, user_type),
+
+	CONSTRAINT fk_user_ID
+		FOREIGN KEY (user_ID)
+		REFERENCES User(user_ID),
+	CONSTRAINT fk_subject_ID
+        FOREIGN KEY (subject_ID)
+        REFERENCES Subject(subject_ID)
+);
