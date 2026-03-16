aadf49c5ed32e0835f9a171d9943a28d81eb47ea
JBeresin <jordyn.beresin@maine.edu>
2026-03-16 18:08:20 -0400

    Update mentorship_entity.sql

--- sql/schema/mentorship_entity.sql
+++ sql/schema/mentorship_entity.sql
@@ -1,7 +1,11 @@
+-- Create mentorship table
 CREATE TABLE Mentorship(
-MentorshipID BINARY(16) PRIMARY KEY,
-SubjectID BINARY(16) NOT NULL,
-FOREIGN KEY (SubjectID) REFERENCES Subject(SubjectID)
+  MentorshipID BINARY(16) PRIMARY KEY,
+  SubjectID BINARY(16) NOT NULL,
+  FOREIGN KEY (SubjectID) REFERENCES Subject(SubjectID)
+
+  ON DELETE CASCADE
+  ON UPDATE CASCADE
 );
 
 -- Create index for SubjectID in mentorship table
