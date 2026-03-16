0a84eecf8a4718bcb4b6cc9f5b9bf3b0e946e588
JBeresin <jordyn.beresin@maine.edu>
2026-03-16 18:09:29 -0400

    Update mentorship_member_entity.sql

--- sql/schema/mentorship_member_entity.sql
+++ sql/schema/mentorship_member_entity.sql
@@ -1,13 +1,19 @@
 -- Create MentorshipMentor table
 CREATE TABLE MentorshipMember(
-MentorshipID BINARY(16) NOT NULL,
-UserID BINARY(16) NOT NULL,
-ROLE ENUM('Mentor', 'Mentee') NOT NULL,
+  MentorshipID BINARY(16) NOT NULL,
+  UserID BINARY(16) NOT NULL,
+  ROLE ENUM('Mentor', 'Mentee') NOT NULL,
 
-PRIMARY KEY(MentorshipID, UserID),
+  -- Designate primary key
+  PRIMARY KEY(MentorshipID, UserID),
 
-FOREIGN KEY (MentorshipID) REFERENCES Mentorship(MentorshipID),
-FOREIGN KEY (UserID) REFERENCES User(UserID));
+  -- Designate foreign key
+  FOREIGN KEY (MentorshipID) REFERENCES Mentorship(MentorshipID),
+  FOREIGN KEY (UserID) REFERENCES User(UserID)
+
+  ON DELETE CASCADE
+  ON UPDATE CASCADE
+  );
 
 -- Check that a user has the role of mentor or mentee
 ALTER TABLE MentorshipMember
