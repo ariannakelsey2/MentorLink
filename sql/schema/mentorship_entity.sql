d9ceb639a25d0621505ed5f9ea43f9f044d061ac
Arianna Kelsey <126092061+ariannakelsey2@users.noreply.github.com>
2026-03-16 22:02:53 -0400

    Enhance Mentorship table with UUID and indexing
    Added default UUID for MentorshipID and created index for Status to support EndMentorship procedure.
    
    -Also updated for naming consistencies

--- sql/schema/mentorship_entity.sql
+++ sql/schema/mentorship_entity.sql
@@ -1,13 +1,28 @@
 -- Create mentorship table
 CREATE TABLE Mentorship(
-  MentorshipID BINARY(16) PRIMARY KEY,
+  MentorshipID BINARY(16) NOT NULL
+    --Added: To default UUID for Mentorship
+    DEFAULT (UUID_TO_BIN(UUID())),
   SubjectID BINARY(16) NOT NULL,
-  FOREIGN KEY (SubjectID) REFERENCES Subject(SubjectID)
+  --Added: Supports Procedure EndMentorship without an actual deletion, allows for archived setting
+  -- However, will need to update ER Diagram and Data Dictionary 
+  Status ENUM('Active','Ended') NOT NULL DEFAULT 'Active',
 
-  ON DELETE CASCADE
-  ON UPDATE CASCADE
+  CONSTRAINT pk_mentorship
+    PRIMARY KEY (MentorshipID),
+  --Style Fix: Nothing wrong with putting primary key next to the declaration, but
+  -- for consistency within the system and readability, I used the constraint style to define the primary and foreign keys
+  CONSTRAINT fk_mentorship_subject
+    FOREIGN KEY (SubjectID)
+    REFERENCES Subject(SubjectID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 -- Create index for SubjectID in mentorship table
 CREATE INDEX idx_Mentorship_SubjectID
 ON Mentorship(SubjectID);
+
+--Added: Created index to help with the procedure function EndMentorship
+CREATE INDEX idx_Mentorship_Status
+    ON Mentorship(Status);
