9983b1001e44dae02f5d691e1df773529d1d341b
Arianna Kelsey <126092061+ariannakelsey2@users.noreply.github.com>
2026-03-22 09:09:51 -0400

    Modify Session table structure and comments
    Added a session status column and updated comments for clarity.

--- sql/schema/session_entity.sql
+++ sql/schema/session_entity.sql
@@ -1,6 +1,14 @@
+USE MentorLink;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS Session;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
 CREATE TABLE Session (
     SessionID BINARY(16) DEFAULT (UUID_TO_BIN(UUID())),
-    --Added Mentorship ID
+    -- Added Mentorship ID
     MentorshipID BINARY(16) NOT NULL,
     Timestamp TIMESTAMP NOT NULL,
     InstructionType ENUM('Virtual', 'In-Person') NOT NULL,
@@ -9,7 +17,7 @@ CREATE TABLE Session (
     -- However, will need to update ER Diagram and Data Dictionary 
     Status ENUM('Scheduled', 'Cancelled', 'Completed') NOT NULL DEFAULT 'Scheduled',
 
-    --Added constraint style to keep with consistency and readability
+    -- Added constraint style to keep with consistency and readability
     CONSTRAINT pk_session
         PRIMARY KEY (SessionID),
 
@@ -20,7 +28,7 @@ CREATE TABLE Session (
         ON UPDATE CASCADE
 );
 
---Added: To help with procedures
+-- Added: To help with procedures
 CREATE INDEX idx_session_mentorship
     ON Session(MentorshipID);
 
