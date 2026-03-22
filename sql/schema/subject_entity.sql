9488984ba873fc05262b61913f4aa3b350641717
Arianna Kelsey <126092061+ariannakelsey2@users.noreply.github.com>
2026-03-22 09:08:24 -0400

    Update subject_entity.sql

--- sql/schema/subject_entity.sql
+++ sql/schema/subject_entity.sql
@@ -1,14 +1,17 @@
+USE MentorLink;
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS Subject;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
 CREATE TABLE Subject (
     -- Fixed: PK defined twice
     SubjectID BINARY(16) DEFAULT (UUID_TO_BIN(UUID())),
     SubjectName VARCHAR(64) NOT NULL UNIQUE,
 
-    --Fixed: Added constraints to enforce data integrity
+    -- Fixed: Added constraints to enforce data integrity
     CONSTRAINT pk_subject
-        PRIMARY KEY (SubjectID),
+        PRIMARY KEY (SubjectID)
     
-   -- Jordie: MySQL gives me a warning that this constraint is redundant because we already specified that SubjectName is unique in line 4
-    CONSTRAINT uq_subject_name
-        UNIQUE (SubjectName)
-
 );
