20cd75e89842c0f395b0891dc4cf4b9fc953e014
Arianna Kelsey <126092061+ariannakelsey2@users.noreply.github.com>
2026-03-31 17:52:39 -0400

    Update submit_rating.sql

--- sql/procedures/arianna_dev/submit_rating.sql
+++ sql/procedures/arianna_dev/submit_rating.sql
@@ -33,9 +33,9 @@ BEGIN
           ON m1.MentorshipID = m2.MentorshipID
         WHERE m1.MentorshipID = v_MentorshipID
           AND m1.UserID = v_RaterUserID
-          AND m1.RoleID = 'Mentee'
+          AND m1.RoleVaue = 'Mentee'
           AND m2.UserID = v_RatedUserID
-          AND m2.RoleID = 'Mentor'
+          AND m2.RoleValue = 'Mentor'
     ) THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Only mentees can rate mentors';
